9f70349e3001b56f9722bc329510e8e33d0c85fd
Geoffrey Thompson <gzthompson@gmail.com>
2018-06-15 15:56:20 -0500

    fixing some documentation

--- R/tea.R
+++ R/tea.R
@@ -1,16 +1,19 @@
 
-#' Play a Sound After Some Time
+#' @title Play a Sound After Some Time
 #'
-#' teatimer is a timer that, by default, runs for however many minutes specified and then
-#' plays a sound when the time is up. It can be set to run in seconds rather than minutes
-#' and the sound can also be specified. This is not a precision timer.
-#' If \code{beepr::beep} is not able to play the sound a warning is issued rather than
+#' @description This is a one-function package that will play a sound after a specified interval of time.
+#' It wraps around the function \code{beepr::beep()}. After a specified number of
+#' minutes or seconds it plays whatever sound is specified. Because it uses
+#' \code{Sys.sleep()} and audio may not play immediately, timing may not be precise.
+#' This is, however, sufficiently accurate for use on a cup of tea.
+#' If \code{beepr::beep()} is not able to play the sound a warning is issued rather than
 #' an error.
 #'
 #' @param time A positive number, the amount of time to wait
 #' @param minutes If TRUE, time is in minutes. If FALSE, time is in seconds.
 #' @param sound character string or number specifying what sound to be played by either specifying
-#'      one of the built in sounds, specifying the path to a wav file or specifying an url. The default is 0.
+#'      one of the built in sounds, specifying the path to a wav file or specifying an url. The default is 0,
+#'      which plays a random sound from \code{beepr::beep()}..
 #'      See the documentation for \code{\link[beepr:beep]{beep}}, as the input on this argument is passed
 #'      directly to that function.
 #'
--- R/teatimer-package.r
+++ R/teatimer-package.r
@@ -1,11 +1,14 @@
-#' Timer With an Alarm
+#' @title Play a Sound After Some Time
+#'
 #'
 #' This is a one-function package that will play a sound after a specified interval of time.
 #' It wraps around the function \code{beepr::beep()}. After a specified number of
 #' minutes or seconds it plays whatever sound is specified. Because it uses
 #' \code{Sys.sleep()} and audio may not play immediately, timing may not be precise.
 #' This is, however, sufficiently accurate for use on a cup of tea.
+#' If \code{beepr::beep()} is not able to play the sound a warning is issued rather than
+#' an error.
 #'
-#' @name teatimer
+#' @name teatimer-package
 #' @docType package
 NULL
